0ee75f1ee19369f9ad756838e8d93545170b657c
Unathi Skosana <ukskosana@gmail.com>
2020-12-27 09:40:06 +0200

    Multiple rm swap select

--- .config/fish/functions/misc.fish
+++ .config/fish/functions/misc.fish
@@ -20,6 +20,19 @@ function new_folder_from_template
     cp -RT $HOME/Templates/$argv[1] $argv[2]
 end
 
+function rm_swap
+  cd /home/lynx/.config/nvim/swap
+  set -l swap_fn (fd -t f -H | fzf -m --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)
+
+  if test -n "$swap_fn"
+    rm $swap_fn
+    echo "$swap_fn deleted successfully"
+  else
+    echo "No file selected"
+  end
+  cd $dirprev[-1]
+end
+
 function open_with_fzf
     cd $HOME
     fd -t f -H -I | fzf -m --preview="xdg-mime query default {}" | xargs -ro -d "\n" xdg-open 2>&-
@@ -37,17 +50,4 @@ end
 
 function notes
     nvim -c "Notes $args"
-end
-
-function rm_swap
-  cd /home/lynx/.config/nvim/swap
-  set -l swap_fn (fd -t f -H | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview" --preview-window=:hidden)
-
-  if test -n "$swap_fn"
-    rm $swap_fn
-    echo "$swap_fn deleted successfully"
-  else
-    echo "No file selected"
-  end
-  cd $dirprev[-1]
 end
